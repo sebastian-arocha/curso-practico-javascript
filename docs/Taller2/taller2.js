741cce6e55f51cf50ba296888b02fe4dd3c5a8cf
arochaking <arochaking@gmail.com>
2021-08-14 13:26:02 -0400

    Terminamos el taller 2

--- docs/Taller2/taller2.js
+++ docs/Taller2/taller2.js
@@ -16,5 +16,16 @@
 function calculoPrecioFinal(precio, descuento) {
   const porcentaje = 100 - descuento;
   const precioFinal = (precio * porcentaje) / 100;
-  console.log("El precio final es " + precioFinal);
+  const resultado = document.getElementById("resultado");
+  resultado.innerText = "El precio final de tu producto es $" + precioFinal;
+}
+
+//Función que llamaremos al darle click al botón en HTML
+
+function precioFinal() {
+  const inputPrecio = document.getElementById("inputPrecio");
+  const inputDescuento = document.getElementById("inputDescuento");
+  const precio = Number(inputPrecio.value);
+  const descuento = Number(inputDescuento.value);
+  const precioconDescuento = calculoPrecioFinal(precio, descuento);
 }
--- docs/Taller2/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-function CalculoPrecioFinal(precio, descuento) {
-  const porcentaje = 100 - descuento;
-  const precioFinal = (precio * porcentaje) / 100;
-  return precioFinal;
-}
